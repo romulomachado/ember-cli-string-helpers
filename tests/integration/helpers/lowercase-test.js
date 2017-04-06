5abe266863fe8364fab9c8096b842e11a275284c
Fred <fpalluel@gmail.com>
2017-04-06 21:12:51 +0200

    Add integration tests

---
+++ tests/integration/helpers/lowercase-test.js
@@ -0,0 +1,54 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('lowercase', 'Integration | Helper | {{lowercase}}', {
+  integration: true
+});
+
+test('It converts all uppercase to lowercase', function(assert) {
+  this.render(hbs`{{lowercase "ALL UPPERCASE"}}`);
+
+  let expected = 'all uppercase';
+
+  assert.equal(this.$().text().trim(), expected, 'converts all uppercase to lowercase');
+});
+
+test('It converts mixed case to lowercase', function(assert) {
+  this.render(hbs`{{lowercase "UPPER and lower CaSe"}}`);
+
+  let expected = 'upper and lower case';
+
+  assert.equal(this.$().text().trim(), expected, 'converts upper and lower case to lowercase');
+});
+
+test('It leaves special characters unmodified', function(assert) {
+  this.render(hbs`{{lowercase "SPECIAL @&\/*-+^\`"}}`);
+
+  let expected = 'special @&/*-+^`';
+
+  assert.equal(this.$().text().trim(), expected, 'converts special characters');
+});
+
+test('It converts accented characters to lowercase', function(assert) {
+  this.render(hbs`{{lowercase "ÂÊÎÔÛÄËÏÖÜÉÀÈ"}}`);
+
+  let expected = 'âêîôûäëïöüéàè';
+
+  assert.equal(this.$().text().trim(), expected, 'converts accented characters to lowercase');
+});
+
+test('It correctly handles empty string input', function(assert) {
+  this.render(hbs`{{lowercase ""}}`);
+
+  let expected = '';
+
+  assert.equal(this.$().text().trim(), expected, 'renders empty string if input is empty string');
+});
+
+test('It correctly handles undefined input', function(assert) {
+  this.render(hbs`{{lowercase undefined}}`);
+
+  let expected = '';
+
+  assert.equal(this.$().text().trim(), expected, 'renders empty string if undefined input');
+});
