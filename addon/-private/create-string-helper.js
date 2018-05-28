7be764d0dc9180d0f3f42650d85902cc885786c3
Markus Mühlberger <markus@mmuehlberger.com>
2018-05-28 18:13:02 +0200

    Add SafeString-safety for all functions

--- addon/-private/create-string-helper.js
+++ addon/-private/create-string-helper.js
@@ -1,5 +1,11 @@
+import { isHtmlSafe } from '@ember/string';
+
 export default function(stringFunction) {
   return function([string]) {
+    if (isHTMLSafe(string)) {
+      string = string.string;
+    }
+
     string = string || '';
     return stringFunction(string);
   };
